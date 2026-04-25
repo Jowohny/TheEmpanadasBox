b983111c1d1b2356e4fd6693fb72f3adf98944fd
Jowohny <vujohny109238@gmail.com>
2026-04-24 23:33:34 -0700

    moved data list objects
    for cleaner code

---
+++ src/data/Links.tsx
@@ -0,0 +1,18 @@
+export interface Link {
+	label: string
+	path: string
+}
+
+const Links = [
+	{ label: 'Home', path: '/' },
+	{ label: 'Menu', path: '/Menu' },
+	{ label: 'Order Pickup', path: '/OrderPickup' },
+	{ label: 'Catering', path: '/Catering' },
+	{ label: 'Events', path: '/Events' },
+	{ label: 'Rewards', path: '/Rewards' },
+	{ label: 'Ship Nationwide', path: '/ShipNationwide' },
+	{ label: 'Contact Us', path: '/ContactUs' },
+	{ label: 'About Us', path: '/AboutUs' },
+];
+
+export default Links
---
+++ src/data/Media.tsx
@@ -0,0 +1,11 @@
+export interface Media {
+	name: string
+	link: string
+}
+
+const SocialMedia: Media[] = [
+	{ name: 'Instagram', link: 'https://www.instagram.com/theempanadasbox/' },
+	{ name: 'Facebook', link: 'https://www.facebook.com/theempanadasbox/'}
+]
+
+export default SocialMedia
